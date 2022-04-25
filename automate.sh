5c6de559e9ca53eeb3e13a7a34e63a0956499735
Austin Hunt <huntaj@g.cofc.edu>
2022-04-25 16:48:08 -0400

    update automation structure; remove the startdev.sh script

--- automate.sh
+++ automate.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Script to automate the execution of nuXmv symbolic model checking against all component models (component/*/check.smv)
-cd ..
 currpath=$(pwd)
 for component in $(ls components/); do
     echo "Checking model for component $component";
     sleep 1;
     path="${currpath}/components/$component/check.smv";
-    logpath="automate/logs/${component}.log"
+    logpath="logs/${component}.log"
     ./nuxmv $path > $logpath
     echo "Results written to $logpath";
     echo ""; echo "";
--- startdev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nuxmvbinpath="nuXmv-2.0.0-Darwin/bin/nuXmv"
-export PATH="$PATH:$(pwd)/$nuxmvbinpath"
